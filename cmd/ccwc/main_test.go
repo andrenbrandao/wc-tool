7df3d9a53f4cfe0855d7aa122bb61ffb422a52d4
André Brandão <andrenbrandao@gmail.com>
2023-07-09 17:49:01 -0300

    test: add more tests for each argument

--- cmd/ccwc/main_test.go
+++ cmd/ccwc/main_test.go
@@ -26,10 +26,48 @@ func TestApp(t *testing.T) {
 		return app.run()
 	}
 
-	got, _ := run([]string{"testdata/data.txt"})
-	want := "8 351 2432 testdata/data.txt"
+	t.Run("without arguments", func(t *testing.T) {
+		got, _ := run([]string{"testdata/data.txt"})
+		want := "8 351 2432 testdata/data.txt"
 
-	if got != want {
-		t.Errorf("got %+v want %+v", got, want)
-	}
+		if got != want {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
+
+	t.Run("with -l", func(t *testing.T) {
+		got, _ := run([]string{"-l", "testdata/data.txt"})
+		want := "8 testdata/data.txt"
+
+		if got != want {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
+
+	t.Run("with -w", func(t *testing.T) {
+		got, _ := run([]string{"-w", "testdata/data.txt"})
+		want := "351 testdata/data.txt"
+
+		if got != want {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
+
+	t.Run("with -c", func(t *testing.T) {
+		got, _ := run([]string{"-c", "testdata/data.txt"})
+		want := "2432 testdata/data.txt"
+
+		if got != want {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
+
+	t.Run("with -m", func(t *testing.T) {
+		got, _ := run([]string{"-m", "testdata/data.txt"})
+		want := "2430 testdata/data.txt"
+
+		if got != want {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+	})
 }
